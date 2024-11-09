09d65db1e05d543c303db4cc736228ffb2365314
MetrixXS <mdumbrava495@gmail.com>
2024-11-09 11:59:12 +0200

    Hi

--- ДЗ 2.py
+++ ДЗ 2.py
@@ -1,10 +1,1 @@
-name = input("Надрокуй свое ім'я:")
-age = input("Надрокуй свій вік:")
-print("Привет ", name, "тебе", age)
-
-g = 18
-age2 = input("Надрокуй свій вік:")
-if age2 > g :
-    print("Вхід відкрит!")
-else:
-    print("Вхід заблоковано!")
+print ("Hello")
