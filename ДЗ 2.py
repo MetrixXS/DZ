96f28d93e7f3d612a16d6378b45e4df2f50d4a47
MetrixXS <mdumbrava495@gmail.com>
2024-11-09 11:55:50 +0200

    DZ2

--- ДЗ 2.py
+++ ДЗ 2.py
@@ -1,1 +1,10 @@
-print ("Heloo")
+name = input("Надрокуй свое ім'я:")
+age = input("Надрокуй свій вік:")
+print("Привет ", name, "тебе", age)
+
+g = 18
+age2 = input("Надрокуй свій вік:")
+if age2 > g :
+    print("Вхід відкрит!")
+else:
+    print("Вхід заблоковано!")
